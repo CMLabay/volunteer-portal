766a741dea8977dff9d878ca909b44bebde50bda
Christina Labay <Christina.m.labay@gmail.com>
2019-07-04 20:45:51 -0500

    change url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default{
     API_KEY: process.env.REACT_APP_API_KEY,
     TOKEN_KEY: 'volunteer-client-auth-token',
-    API_ENDPOINT: 'http://localhost:8000/api',
+    API_ENDPOINT: 'https://still-plateau-82205.herokuapp.com/api',
 }
